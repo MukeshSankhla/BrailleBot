039ee2379c23a0ac13d9868e987a6bb23fb71b70
Mukesh Sankhla <48177784+MukeshSankhla@users.noreply.github.com>
2023-11-28 22:36:45 +0530

    Update BrailleBot.ino

--- BrailleBot.ino
+++ BrailleBot.ino
@@ -4,8 +4,11 @@
 
 #define FPSerial Serial1
 
-Servo myservo = Servo();
+#define pause 5000
 
+int position = 1;
+
+Servo myservo = Servo();
 DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
 
@@ -27,288 +30,421 @@ void setup() {
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
   }
+
   Serial.println(F("Lets Start Learning."));
-  
+
+  clear();
+  delay(1000);
+
   myDFPlayer.volume(30);
   myDFPlayer.play(27);
-  delay(5000); 
+  delay(4000);
+
+  for(int i = 0; i< 5; i++){
+    up();
+    clear();
+  }
 }
 
 void loop() {
-  a();
+  if(position == 1){
+    a();
+  }
+  if(position == 2){
+    b();
+  }
+  if(position == 3){
+    c();
+  }
+  if(position == 4){
+    d();
+  }
+  if(position == 5){
+    e();
+  }
+  if(position == 6){
+    f();
+  }
+  if(position == 7){
+    g();
+  }
+  if(position == 8){
+    h();
+  }
+  if(position == 9){
+    i();
+  }
+  if(position == 10){
+    j();
+  }
+  if(position == 11){
+    k();
+  }
+  if(position == 12){
+    l();
+  }
+  if(position == 13){
+    m();
+  }
+  if(position == 14){
+    n();
+  }
+  if(position == 15){
+    o();
+  }
+  if(position == 16){
+    p();
+  }
+  if(position == 17){
+    q();
+  }
+  if(position == 18){
+    r();
+  }
+  if(position == 19){
+    s();
+  }
+  if(position == 20){
+    t();
+  }
+  if(position == 21){
+    u();
+  }
+  if(position == 22){
+    v();
+  }
+  if(position == 23){
+    w();
+  }
+  if(position == 24){
+    x();
+  }
+  if(position == 25){
+    y();
+  }
+  if(position == 26){
+    z();
+  }
+  if(position == 27){
+    position == 1;
+  }
+  position++;
+  clear();
 }
 
-void zero(){
-  myservo.write(servo6, 0);
+void dot1Up(){
+  for (int pos = 180; pos >= 162; pos--) {
+    myservo.write(servo1, pos);  
+    delay(5);
+  }
+}
+void dot1Down(){
+  for (int pos = 162; pos <= 180; pos++) {
+    myservo.write(servo1, pos);  
+    delay(5);
+  }
+}
+
+void dot2Up(){
+  for (int pos = 0; pos <= 20; pos++) {
+    myservo.write(servo2, pos);  
+    delay(5);
+  }
+}
+void dot2Down(){
+  for (int pos = 20; pos >= 0; pos--) {
+    myservo.write(servo2, pos);  
+    delay(5);
+  }
+}
+
+void dot3Up(){
+  for (int pos = 180; pos >= 165; pos--) {
+    myservo.write(servo3, pos);  
+    delay(5);
+  }
+}
+void dot3Down(){
+  for (int pos = 165; pos <= 180; pos++) {
+    myservo.write(servo3, pos);  
+    delay(5);
+  }
+}
+
+void dot4Up(){
+  for (int pos = 0; pos <= 16; pos++) {
+    myservo.write(servo4, pos);  
+    delay(5);
+  }
+}
+void dot4Down(){
+  for (int pos = 16; pos >= 0; pos--) {
+    myservo.write(servo4, pos);  
+    delay(5);
+  }
+}
+
+void dot5Up(){
+  for (int pos = 180; pos >= 160; pos--) {
+    myservo.write(servo5, pos);  
+    delay(5);
+  }
+}
+void dot5Down(){
+  for (int pos = 160; pos <= 180; pos++) {
+    myservo.write(servo5, pos);  
+    delay(5);
+  }
+}
+
+void dot6Up(){
+  for (int pos = 0; pos <= 10; pos++) {
+    myservo.write(servo6, pos);  
+    delay(5);
+  }
+}
+void dot6Down(){
+  for (int pos = 10; pos >= 0; pos--) {
+    myservo.write(servo6, pos);  
+    delay(5);
+  }
 }
 
 void clear(){
-    myservo.write(servo1, 0);
-    myservo.write(servo2, 0); 
-    myservo.write(servo3, 0); 
-    myservo.write(servo4, 0); 
-    myservo.write(servo5, 0); 
-    myservo.write(servo6, 0);
+  dot1Down();
+  delay(100);
+  dot2Down();
+  delay(100);
+  dot3Down();
+  delay(100);
+  dot4Down();
+  delay(100);
+  dot5Down();
+  delay(100);
+  dot6Down();
+  delay(100);
 }
 
 void a(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);  
-    delay(10);
-  }
-  Serial.println(F("A"));
+  dot1Up();
   myDFPlayer.play(2);
-  delay(5000);
-  for (int pos = 10; pos >= 0; pos--) {
-    myservo.write(servo1, pos);        
-    delay(10);
-  }
-  delay(1000);
+  delay(pause);
 }
 
 void b(){
-  for (int pos = 0; pos <= 17; pos++) {
-    myservo.write(servo3, pos);  
-    delay(10);
-  }
-  Serial.println(F("A"));
+  dot1Up();
+  dot3Up();
   myDFPlayer.play(3);
-  delay(5000);
-  for (int pos = 17; pos >= 0; pos--) {
-    myservo.write(servo3, pos);        
-    delay(10);
-  }
-  delay(1000);
+  delay(pause);
 }
 
 void c(){
-  for (int pos = 0; pos <= 17; pos++) {
-    myservo.write(servo2, pos);  
-    delay(10);
-  }
-  Serial.println(F("A"));
+  dot1Up();
+  dot2Up();
   myDFPlayer.play(4);
-  delay(5000);
-  for (int pos = 17; pos >= 0; pos--) {
-    myservo.write(servo2, pos);        
-    delay(10);
-  }
-  delay(1000);
+  delay(pause);
 }
 
 void d(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo4, pos);   
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot4Up();
+  myDFPlayer.play(5);
+  delay(pause);
 }
 
 void e(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);   
-    myservo.write(servo4, pos);   
-    delay(10);
-  }
+  dot1Up();
+  dot4Up();
+  myDFPlayer.play(6);
+  delay(pause);
 }
 
 void f(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo3, pos);   
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot3Up();
+  myDFPlayer.play(7);
+  delay(pause);
 }
 
 void g(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot3Up();
+  dot4Up();
+  myDFPlayer.play(8);
+  delay(pause);
 }
 
 void h(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot3Up();
+  dot4Up();
+  myDFPlayer.play(9);
+  delay(pause);
 }
 
 void i(){
-  for (int pos = 0; pos <= 10; pos++) {   
-    myservo.write(servo2, pos);   
-    myservo.write(servo3, pos);  
-    delay(10);
-  }
+  dot2Up();
+  dot3Up();
+  myDFPlayer.play(10);
+  delay(pause);
 }
 
 void j(){
-  for (int pos = 0; pos <= 10; pos++) {  
-    myservo.write(servo2, pos);   
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos); 
-    delay(10);
-  }
+  dot2Up();
+  dot3Up();
+  dot4Up();
+  myDFPlayer.play(11);
+  delay(pause);
 }
 
 void k(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo5, pos);  
-    delay(10);
-  }
+  dot1Up();
+  dot5Up();
+  myDFPlayer.play(12);
+  delay(pause);
 }
 
 void l(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);   
-    myservo.write(servo3, pos);   
-    myservo.write(servo5, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot3Up();
+  dot5Up();
+  myDFPlayer.play(13);
+  delay(pause);
 }
 
 void m(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo5, pos);   
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot5Up();
+  myDFPlayer.play(14);
+  delay(pause);
 }
 
 void n(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo4, pos);   
-    myservo.write(servo5, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot4Up();
+  dot5Up();
+  myDFPlayer.play(15);
+  delay(pause);
 }
 
 void o(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo4, pos);   
-    myservo.write(servo5, pos);   
-    delay(10);
-  }
+  dot1Up();
+  dot4Up();
+  dot5Up();
+  myDFPlayer.play(16);
+  delay(pause);
 }
 
 void p(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo3, pos);   
-    myservo.write(servo5, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot3Up();
+  dot5Up();
+  myDFPlayer.play(17);
+  delay(pause);
 }
 
 void q(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos);
-    myservo.write(servo5, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot3Up();
+  dot4Up();
+  dot5Up();
+  myDFPlayer.play(18);
+  delay(pause);
 }
 
 void r(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos);   
-    myservo.write(servo5, pos);
-    delay(10);
-  }
+  dot1Up();
+  dot3Up();
+  dot4Up();
+  dot5Up();
+  myDFPlayer.play(19);
+  delay(pause);
 }
 
 void s(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo2, pos);    
-    myservo.write(servo3, pos);   
-    myservo.write(servo5, pos);   
-    delay(10);
-  }
+  dot2Up();
+  dot3Up();
+  dot5Up();
+  myDFPlayer.play(20);
+  delay(pause);
 }
 
 void t(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo2, pos);    
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos);  
-    myservo.write(servo5, pos); 
-    delay(10);
-  }
+  dot2Up();
+  dot3Up();
+  dot4Up();
+  dot5Up();
+  myDFPlayer.play(21);
+  delay(pause);
 }
 
 void u(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo5, pos);   
-    myservo.write(servo6, pos);   
-    delay(10);
-  }
+  dot1Up();
+  dot5Up();
+  dot6Up();
+  myDFPlayer.play(22);
+  delay(pause);
 }
 
 void v(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo3, pos);   
-    myservo.write(servo5, pos);   
-    myservo.write(servo6, pos);
-    delay(10);
-  }
+  dot1Up();
+  dot3Up();
+  dot5Up();
+  dot6Up();
+  myDFPlayer.play(23);
+  delay(pause);
 }
 
 void w(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo2, pos);    
-    myservo.write(servo3, pos);   
-    myservo.write(servo4, pos);   
-    myservo.write(servo6, pos);
-    delay(10);
-  }
+  dot2Up();
+  dot3Up();
+  dot4Up();
+  dot6Up();
+  myDFPlayer.play(24);
+  delay(pause);
 }
 
 void x(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo5, pos);   
-    myservo.write(servo6, pos);
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot5Up();
+  dot6Up();
+  myDFPlayer.play(25);
+  delay(pause);
 }
 
 void y(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo2, pos);   
-    myservo.write(servo4, pos);   
-    myservo.write(servo5, pos);
-    myservo.write(servo6, pos); 
-    delay(10);
-  }
+  dot1Up();
+  dot2Up();
+  dot4Up();
+  dot5Up();
+  dot6Up();
+  myDFPlayer.play(26);
+  delay(pause);
 }
 
 void z(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo1, pos);    
-    myservo.write(servo4, pos);   
-    myservo.write(servo5, pos);   
-    myservo.write(servo6, pos);
-    delay(10);
-  }
+  dot1Up();
+  dot4Up();
+  dot5Up();
+  dot6Up();
+  myDFPlayer.play(1);
+  delay(pause);
+}
+
+
+void up(){
+  dot1Up();
+  dot2Up();
+  dot3Up();
+  dot4Up();
+  dot5Up();
+  dot6Up();
 }
