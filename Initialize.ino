e68df1bcca46c280d318c80a7e5dea5d444f8c35
Mukesh Sankhla <48177784+MukeshSankhla@users.noreply.github.com>
2023-11-28 22:37:11 +0530

    Update Initialize.ino

--- Initialize.ino
+++ Initialize.ino
@@ -14,14 +14,16 @@ const int servo5 = 15;
 const int servo6 = 16;
 
 void setup() {
-  for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
-    myservo.write(servo1, pos);        
-    delay(15);
-  }
+
   for (int pos = 180; pos >= 0; pos--) {  // go from 180-0 degrees
     myservo.write(servo1, pos);       
     delay(15);
   }
+  for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
+    myservo.write(servo1, pos);        
+    delay(15);
+  }
+
   for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
     myservo.write(servo2, pos);        
     delay(15);
@@ -30,14 +32,16 @@ void setup() {
     myservo.write(servo2, pos);       
     delay(15);
   }
-  for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
-    myservo.write(servo3, pos);        
-    delay(15);
-  }
+
   for (int pos = 180; pos >= 0; pos--) {  // go from 180-0 degrees
     myservo.write(servo3, pos);       
     delay(15);
   }
+  for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
+    myservo.write(servo3, pos);        
+    delay(15);
+  }
+
   for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
     myservo.write(servo4, pos);        
     delay(15);
@@ -46,14 +50,16 @@ void setup() {
     myservo.write(servo4, pos);       
     delay(15);
   }
-  for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
-    myservo.write(servo5, pos);        
-    delay(15);
-  }
+
   for (int pos = 180; pos >= 0; pos--) {  // go from 180-0 degrees
     myservo.write(servo5, pos);       
     delay(15);
   }
+  for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
+    myservo.write(servo5, pos);        
+    delay(15);
+  }
+
   for (int pos = 0; pos <= 180; pos++) {  // go from 180-0 degrees
     myservo.write(servo6, pos);        
     delay(15);
