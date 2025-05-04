502e03d0674058ad0afbc398509090187bfbf1fb
Mukesh Sankhla <48177784+MukeshSankhla@users.noreply.github.com>
2025-05-04 07:26:50 +0530

    Update BrailleBot.ino

--- BrailleBot.ino
+++ BrailleBot.ino
@@ -4,511 +4,194 @@
    Website  :  makerbrains.com
 */
 
-#include <Servo.h>
+#include <ESP32Servo.h> //Search in Library Manager
 #include "Arduino.h"
 #include "DFRobotDFPlayerMini.h"
 
 #define FPSerial Serial1
-
-// The delay of 2sec for the action to complete
 #define pause 2000
 
-// Pin assignments for "Next" and "Previous" buttons
 const int nextPin = 45;
 const int previousPin = 8;
 
-// Flags to detect button presses
 volatile bool nextPressed = false;
 volatile bool previousPressed = false;
 
-int position = 0;  // Current Braille alphabet position
+int position = 0;
 
-// Servo motor assignments for Braille dots
-Servo myservo = Servo();
-const int servo1 = 4;
-const int servo2 = 5;
-const int servo3 = 6;
-const int servo4 = 7;
-const int servo5 = 15;
-const int servo6 = 16;
+// Declare one Servo object per Braille dot
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
 
-// Declaration of the DFPlayer instance
+const int pin1 = 4;
+const int pin2 = 5;
+const int pin3 = 6;
+const int pin4 = 7;
+const int pin5 = 15;
+const int pin6 = 16;
+
 DFRobotDFPlayerMini myDFPlayer;
 
 void setup() {
   pinMode(nextPin, INPUT_PULLUP);
   pinMode(previousPin, INPUT_PULLUP);
 
-  // Set up serial communication
-  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/48, /*tx =*/46);
+  // Attach servos to their pins
+  servo1.attach(pin1);
+  servo2.attach(pin2);
+  servo3.attach(pin3);
+  servo4.attach(pin4);
+  servo5.attach(pin5);
+  servo6.attach(pin6);
+
+  FPSerial.begin(9600, SERIAL_8N1, 48, 46);
   Serial.begin(115200);
 
-  // Display welcome message
   Serial.println(F("Hello! I am BrailleBot."));
 
-  // Attach interrupts for button presses
   attachInterrupt(digitalPinToInterrupt(nextPin), nextISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(previousPin), previousISR, FALLING);
 
-  // Initialize the DFPlayer module
-  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {
+  if (!myDFPlayer.begin(FPSerial, true, true)) {
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1. Please recheck the connection!"));
     Serial.println(F("2. Please insert the SD card!"));
   }
 
-  // Display start message
   Serial.println(F("Let's Start Learning."));
 
-  // Clear Braille display and play introduction audio
   clear();
   delay(1000);
   myDFPlayer.volume(30);
   myDFPlayer.play(27);
   delay(4000);
 
-  // Perform initial setup by moving Braille dots up and down
   for(int i = 0; i < 5; i++){
     up();
     clear();
   }
-  play();  // Play the audio for the current Braille letter
+
+  play();
 }
 
-// Main loop
 void loop() {
-  // Check if "Next" button is pressed
   if (nextPressed) {
     position++;
     clear();
-    play();  // Play the audio for the new Braille letter
-    nextPressed = false;  // Reset the flag
+    play();
+    nextPressed = false;
   }
 
-  // Check if "Previous" button is pressed
   if (previousPressed) {
     position--;
     clear();
-    play();  // Play the audio for the new Braille letter
-    previousPressed = false;  // Reset the flag
+    play();
+    previousPressed = false;
   }
 }
 
-// Functions to move each Braille dot up and down
-void dot1Up(){
-  for (int pos = 180; pos >= 162; pos--) {
-    myservo.write(servo1, pos);  
-    delay(5);
-  }
-}
-void dot1Down(){
-  for (int pos = 162; pos <= 180; pos++) {
-    myservo.write(servo1, pos);  
-    delay(5);
+// Servo control functions
+void dot1Up(){ for (int pos = 180; pos >= 162; pos--) { servo1.write(pos); delay(5); } }
+void dot1Down(){ for (int pos = 162; pos <= 180; pos++) { servo1.write(pos); delay(5); } }
+
+void dot2Up(){ for (int pos = 0; pos <= 20; pos++) { servo2.write(pos); delay(5); } }
+void dot2Down(){ for (int pos = 20; pos >= 0; pos--) { servo2.write(pos); delay(5); } }
+
+void dot3Up(){ for (int pos = 180; pos >= 165; pos--) { servo3.write(pos); delay(5); } }
+void dot3Down(){ for (int pos = 165; pos <= 180; pos++) { servo3.write(pos); delay(5); } }
+
+void dot4Up(){ for (int pos = 0; pos <= 16; pos++) { servo4.write(pos); delay(5); } }
+void dot4Down(){ for (int pos = 16; pos >= 0; pos--) { servo4.write(pos); delay(5); } }
+
+void dot5Up(){ for (int pos = 180; pos >= 160; pos--) { servo5.write(pos); delay(5); } }
+void dot5Down(){ for (int pos = 160; pos <= 180; pos++) { servo5.write(pos); delay(5); } }
+
+void dot6Up(){ for (int pos = 0; pos <= 10; pos++) { servo6.write(pos); delay(5); } }
+void dot6Down(){ for (int pos = 10; pos >= 0; pos--) { servo6.write(pos); delay(5); } }
+
+void play(){
+  if (position <= 0) position = 1;
+  if (position > 27) position = 1;
+
+  switch (position) {
+    case 1: a(); break;
+    case 2: b(); break;
+    case 3: c(); break;
+    case 4: d(); break;
+    case 5: e(); break;
+    case 6: f(); break;
+    case 7: g(); break;
+    case 8: h(); break;
+    case 9: i(); break;
+    case 10: j(); break;
+    case 11: k(); break;
+    case 12: l(); break;
+    case 13: m(); break;
+    case 14: n(); break;
+    case 15: o(); break;
+    case 16: p(); break;
+    case 17: q(); break;
+    case 18: r(); break;
+    case 19: s(); break;
+    case 20: t(); break;
+    case 21: u(); break;
+    case 22: v(); break;
+    case 23: w(); break;
+    case 24: x(); break;
+    case 25: y(); break;
+    case 26: z(); break;
   }
 }
 
-void dot2Up(){
-  for (int pos = 0; pos <= 20; pos++) {
-    myservo.write(servo2, pos);  
-    delay(5);
-  }
-}
-void dot2Down(){
-  for (int pos = 20; pos >= 0; pos--) {
-    myservo.write(servo2, pos);  
-    delay(5);
-  }
-}
-
-void dot3Up(){
-  for (int pos = 180; pos >= 165; pos--) {
-    myservo.write(servo3, pos);  
-    delay(5);
-  }
-}
-void dot3Down(){
-  for (int pos = 165; pos <= 180; pos++) {
-    myservo.write(servo3, pos);  
-    delay(5);
-  }
-}
-
-void dot4Up(){
-  for (int pos = 0; pos <= 16; pos++) {
-    myservo.write(servo4, pos);  
-    delay(5);
-  }
-}
-void dot4Down(){
-  for (int pos = 16; pos >= 0; pos--) {
-    myservo.write(servo4, pos);  
-    delay(5);
-  }
-}
-
-void dot5Up(){
-  for (int pos = 180; pos >= 160; pos--) {
-    myservo.write(servo5, pos);  
-    delay(5);
-  }
-}
-void dot5Down(){
-  for (int pos = 160; pos <= 180; pos++) {
-    myservo.write(servo5, pos);  
-    delay(5);
-  }
-}
-
-void dot6Up(){
-  for (int pos = 0; pos <= 10; pos++) {
-    myservo.write(servo6, pos);  
-    delay(5);
-  }
-}
-void dot6Down(){
-  for (int pos = 10; pos >= 0; pos--) {
-    myservo.write(servo6, pos);  
-    delay(5);
-  }
-}
-
-
-// Function to play audio and move Braille dots for each letter
-void play(){  
-    if(position == 0){
-      position = 1;
-    }
-    if(position == 1){
-      a();
-    }
-    if(position == 2){
-      b();
-    }
-    if(position == 3){
-      c();
-    }
-    if(position == 4){
-      d();
-    }
-    if(position == 5){
-      e();
-    }
-    if(position == 6){
-      f();
-    }
-    if(position == 7){
-      g();
-    }
-    if(position == 8){
-      h();
-    }
-    if(position == 9){
-      i();
-    }
-    if(position == 10){
-      j();
-    }
-    if(position == 11){
-      k();
-    }
-    if(position == 12){
-      l();
-    }
-    if(position == 13){
-      m();
-    }
-    if(position == 14){
-      n();
-    }
-    if(position == 15){
-      o();
-    }
-    if(position == 16){
-      p();
-    }
-    if(position == 17){
-      q();
-    }
-    if(position == 18){
-      r();
-    }
-    if(position == 19){
-      s();
-    }
-    if(position == 20){
-      t();
-    }
-    if(position == 21){
-      u();
-    }
-    if(position == 22){
-      v();
-    }
-    if(position == 23){
-      w();
-    }
-    if(position == 24){
-      x();
-    }
-    if(position == 25){
-      y();
-    }
-    if(position == 26){
-      z();
-    }
-    if(position == 27){
-      position = 1;
-    }
-  }
-
-// Function to clear Braille display by moving all dots down
 void clear(){
-  dot1Down();
-  delay(100);
-  dot2Down();
-  delay(100);
-  dot3Down();
-  delay(100);
-  dot4Down();
-  delay(100);
-  dot5Down();
-  delay(100);
-  dot6Down();
-  delay(100);
+  dot1Down(); delay(100);
+  dot2Down(); delay(100);
+  dot3Down(); delay(100);
+  dot4Down(); delay(100);
+  dot5Down(); delay(100);
+  dot6Down(); delay(100);
 }
 
-// Functions for each Braille letter from A to Z
-void a(){
-  dot1Up();
-  myDFPlayer.play(2);
-  delay(pause);
-}
+void a(){ dot1Up(); myDFPlayer.play(2); delay(pause); }
+void b(){ dot1Up(); dot3Up(); myDFPlayer.play(3); delay(pause); }
+void c(){ dot1Up(); dot2Up(); myDFPlayer.play(4); delay(pause); }
+void d(){ dot1Up(); dot2Up(); dot4Up(); myDFPlayer.play(5); delay(pause); }
+void e(){ dot1Up(); dot4Up(); myDFPlayer.play(6); delay(pause); }
+void f(){ dot1Up(); dot2Up(); dot3Up(); myDFPlayer.play(7); delay(pause); }
+void g(){ dot1Up(); dot2Up(); dot3Up(); dot4Up(); myDFPlayer.play(8); delay(pause); }
+void h(){ dot1Up(); dot3Up(); dot4Up(); myDFPlayer.play(9); delay(pause); }
+void i(){ dot2Up(); dot3Up(); myDFPlayer.play(10); delay(pause); }
+void j(){ dot2Up(); dot3Up(); dot4Up(); myDFPlayer.play(11); delay(pause); }
+void k(){ dot1Up(); dot5Up(); myDFPlayer.play(12); delay(pause); }
+void l(){ dot1Up(); dot3Up(); dot5Up(); myDFPlayer.play(13); delay(pause); }
+void m(){ dot1Up(); dot2Up(); dot5Up(); myDFPlayer.play(14); delay(pause); }
+void n(){ dot1Up(); dot2Up(); dot4Up(); dot5Up(); myDFPlayer.play(15); delay(pause); }
+void o(){ dot1Up(); dot4Up(); dot5Up(); myDFPlayer.play(16); delay(pause); }
+void p(){ dot1Up(); dot2Up(); dot3Up(); dot5Up(); myDFPlayer.play(17); delay(pause); }
+void q(){ dot1Up(); dot2Up(); dot3Up(); dot4Up(); dot5Up(); myDFPlayer.play(18); delay(pause); }
+void r(){ dot1Up(); dot3Up(); dot4Up(); dot5Up(); myDFPlayer.play(19); delay(pause); }
+void s(){ dot2Up(); dot3Up(); dot5Up(); myDFPlayer.play(20); delay(pause); }
+void t(){ dot2Up(); dot3Up(); dot4Up(); dot5Up(); myDFPlayer.play(21); delay(pause); }
+void u(){ dot1Up(); dot5Up(); dot6Up(); myDFPlayer.play(22); delay(pause); }
+void v(){ dot1Up(); dot3Up(); dot5Up(); dot6Up(); myDFPlayer.play(23); delay(pause); }
+void w(){ dot2Up(); dot3Up(); dot4Up(); dot6Up(); myDFPlayer.play(24); delay(pause); }
+void x(){ dot1Up(); dot2Up(); dot5Up(); dot6Up(); myDFPlayer.play(25); delay(pause); }
+void y(){ dot1Up(); dot2Up(); dot4Up(); dot5Up(); dot6Up(); myDFPlayer.play(26); delay(pause); }
+void z(){ dot1Up(); dot4Up(); dot5Up(); dot6Up(); myDFPlayer.play(1); delay(pause); }
 
-void b(){
-  dot1Up();
-  dot3Up();
-  myDFPlayer.play(3);
-  delay(pause);
-}
-
-void c(){
-  dot1Up();
-  dot2Up();
-  myDFPlayer.play(4);
-  delay(pause);
-}
-
-void d(){
-  dot1Up();
-  dot2Up();
-  dot4Up();
-  myDFPlayer.play(5);
-  delay(pause);
-}
-
-void e(){
-  dot1Up();
-  dot4Up();
-  myDFPlayer.play(6);
-  delay(pause);
-}
-
-void f(){
-  dot1Up();
-  dot2Up();
-  dot3Up();
-  myDFPlayer.play(7);
-  delay(pause);
-}
-
-void g(){
-  dot1Up();
-  dot2Up();
-  dot3Up();
-  dot4Up();
-  myDFPlayer.play(8);
-  delay(pause);
-}
-
-void h(){
-  dot1Up();
-  dot3Up();
-  dot4Up();
-  myDFPlayer.play(9);
-  delay(pause);
-}
-
-void i(){
-  dot2Up();
-  dot3Up();
-  myDFPlayer.play(10);
-  delay(pause);
-}
-
-void j(){
-  dot2Up();
-  dot3Up();
-  dot4Up();
-  myDFPlayer.play(11);
-  delay(pause);
-}
-
-void k(){
-  dot1Up();
-  dot5Up();
-  myDFPlayer.play(12);
-  delay(pause);
-}
-
-void l(){
-  dot1Up();
-  dot3Up();
-  dot5Up();
-  myDFPlayer.play(13);
-  delay(pause);
-}
-
-void m(){
-  dot1Up();
-  dot2Up();
-  dot5Up();
-  myDFPlayer.play(14);
-  delay(pause);
-}
-
-void n(){
-  dot1Up();
-  dot2Up();
-  dot4Up();
-  dot5Up();
-  myDFPlayer.play(15);
-  delay(pause);
-}
-
-void o(){
-  dot1Up();
-  dot4Up();
-  dot5Up();
-  myDFPlayer.play(16);
-  delay(pause);
-}
-
-void p(){
-  dot1Up();
-  dot2Up();
-  dot3Up();
-  dot5Up();
-  myDFPlayer.play(17);
-  delay(pause);
-}
-
-void q(){
-  dot1Up();
-  dot2Up();
-  dot3Up();
-  dot4Up();
-  dot5Up();
-  myDFPlayer.play(18);
-  delay(pause);
-}
-
-void r(){
-  dot1Up();
-  dot3Up();
-  dot4Up();
-  dot5Up();
-  myDFPlayer.play(19);
-  delay(pause);
-}
-
-void s(){
-  dot2Up();
-  dot3Up();
-  dot5Up();
-  myDFPlayer.play(20);
-  delay(pause);
-}
-
-void t(){
-  dot2Up();
-  dot3Up();
-  dot4Up();
-  dot5Up();
-  myDFPlayer.play(21);
-  delay(pause);
-}
-
-void u(){
-  dot1Up();
-  dot5Up();
-  dot6Up();
-  myDFPlayer.play(22);
-  delay(pause);
-}
-
-void v(){
-  dot1Up();
-  dot3Up();
-  dot5Up();
-  dot6Up();
-  myDFPlayer.play(23);
-  delay(pause);
-}
-
-void w(){
-  dot2Up();
-  dot3Up();
-  dot4Up();
-  dot6Up();
-  myDFPlayer.play(24);
-  delay(pause);
-}
-
-void x(){
-  dot1Up();
-  dot2Up();
-  dot5Up();
-  dot6Up();
-  myDFPlayer.play(25);
-  delay(pause);
-}
-
-void y(){
-  dot1Up();
-  dot2Up();
-  dot4Up();
-  dot5Up();
-  dot6Up();
-  myDFPlayer.play(26);
-  delay(pause);
-}
-
-void z(){
-  dot1Up();
-  dot4Up();
-  dot5Up();
-  dot6Up();
-  myDFPlayer.play(1);
-  delay(pause);
-}
-
-// Function to move all Braille dots up
 void up(){
-  dot1Up();
-  dot2Up();
-  dot3Up();
-  dot4Up();
-  dot5Up();
-  dot6Up();
+  dot1Up(); dot2Up(); dot3Up();
+  dot4Up(); dot5Up(); dot6Up();
 }
 
-// Interrupt service routine for "Next" button
 void nextISR() {
   nextPressed = true;
 }
 
-// Interrupt service routine for "Previous" button
 void previousISR() {
   previousPressed = true;
 }
